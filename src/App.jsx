7dabba4148d7877eb3a94c9e740d7e06d64a2e46
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-04-19 16:38:06 +0530

    sdfgsdfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import { v4 as uuidv4 } from 'uuid';
 import './App.css'
 
 function App() {
@@ -9,18 +9,21 @@ function App() {
   const [expense,setExpense] = useState("")
   const [amount,setAmount] = useState("")
   const [data,setData] = useState([])
+  const id = uuidv4();
 
  
   function submit(){
     
     console.log(expense ,amount , "done")
 // setData(data)
-    let newData = [
+    let newData = 
       {
         expense:expense,
-        amount:amount
+        amount:amount,
+        id:id
+        
       }
-    ]
+    
     setData((prev)=> [...prev,newData])
 
     console.log(data)
@@ -31,21 +34,62 @@ function App() {
   setExpense("")
  }
 
+
+function dele(id){
+  const deleData = data.filter((ele) => ele.id != id)
+  setData(deleData)
+  // console.log(deleData)
+}
+
   return (
 
 
 
-    <div className='mx-auto text-start max-w-[360px]'>
+    <div className='mx-auto text-start max-w-[360px] '>
+
+      <h1 className='text-3xl font-bold mb-5'>Expense Tracker </h1>
+
+      <div>
+        <h2 className='text-xl font-medium'>Your Balance</h2>
+        {/* total summm */}
+
+
+        <h2 className='text-4xl font-mono font-bold '>
+        ${data.reduce((a,v) => a = a + v.amount,0 ).toFixed(2)}
+
+
+        </h2>
+
+
+      </div>
+
+
+      <div className='my-5 flex justify-evenly shadow px-2 py-6 bg-white rounded-xl text-center '>
+          <div className=' flex-grow border-r'>
+        <h2 className='text-xl font-bold'>Income</h2>
+        <h2 className='text-2xl font-mono text-green-400'>{data.reduce((total,item) => item.amount >0 ? total+ item.amount : total ,0).toFixed(2)}</h2>
+          </div>
+          <div className=' flex-grow'>
+        <h2 className='text-xl font-bold'>Expense</h2>
+        <h2 className='text-2xl  font-mono text-red-400'>{data.reduce((total,item ) => item.amount < 0 ? total+ item.amount : total,0).toFixed(2)}</h2>
+          </div>
+      </div>
 
 <div className='my-5'>
       
 <h3 className='text-xl font-bold'>History</h3>
     <br />
     <hr />
-    <div className='flex flex-col gap-2 mt-1'>
+    <div className='flex flex-col gap-2 mt-3'>
         {
-          data.map((ele,index)=> (<div key={index} className='flex border  justify-between'>
-            <h2 className='bg-orange-500 '>{ele.expense}</h2>
+          data.map((ele)=> (
+          <div key={ele.id} className=" flex  shadow px-3 py-2 rounded-xl  shadow-slate-600  justify-between bg-white">
+            <h2 className=''>{ele.expense}</h2>
+            <div className='flex gap-3'> 
+              <h2 className={ele.amount < 0 ? "text-red-500 font-mono" : "text-green-500  font-mono"}>${ele.amount}</h2>
+              <button type='button' onClick={()=> dele(ele.id)}>❌</button>
+              
+            </div>
           </div>))
         }
     </div>
@@ -66,13 +110,13 @@ function App() {
 <div className='mt-5 flex flex-col'>
 <label htmlFor="" className='text-lg'>Text </label>
 
-<input type="text" onChange={(e)=> setExpense(e.target.value) } value={expense} className='shadow rounded-lg px-3 mt-2 shadow-slate-500 py-2 flex-grow ' />
+<input type="text" onChange={(e)=> setExpense(e.target.value) } value={expense} className='shadow bg-white rounded-lg px-3 mt-2 shadow-slate-500 py-2 flex-grow ' />
 </div>
 <div className='mt-5 flex flex-col'>
 <label htmlFor="" className='text-lg'>Amount </label>
 <label htmlFor="" className='text-sm text-slate-500'>(negative - expense, positive - income)</label>
 
-<input type="number" value={amount} onChange={(e) => setAmount(Number(e.target.value))} className='shadow mt-2 rounded-lg px-3 shadow-slate-500 py-2 flex-grow ' />
+<input type="number" value={amount} required onChange={(e) => setAmount(Number(e.target.value))} className='shadow mt-2 rounded-lg px-3   bg-white shadow-slate-500 py-2 flex-grow ' />
 
 <button type="submit" className="mt-6 text-white flex-grow bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Add transaction</button>
 
