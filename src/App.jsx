28423d21dc783801b557d9060ea2418370307af1
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-04-21 23:46:44 +0530

    dsfg

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
 
   function getLocal(){
-   let local =  JSON.parse(localStorage.getItem("data"));
+   let local =  JSON.parse(localStorage.getItem("data")) || [];
    setData(local)
   }
 
